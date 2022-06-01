8b8e098e121fab58d48a4ca511581206eea6a7b1
barkotenicholas <barkotenicholas@gmail.com>
2022-06-01 13:42:37 +0300

    Added splasha ctivity

--- app/src/main/java/com/barkote/emojify/Splash.kt
+++ app/src/main/java/com/barkote/emojify/Splash.kt
@@ -2,17 +2,20 @@ package com.barkote.emojify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.barkote.emojify.databinding.ActivityMainBinding
+import com.barkote.emojify.databinding.ActivitySplashBinding
 
-class MainActivity : AppCompatActivity() {
+class Splash : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+
+
 
     }
 }
